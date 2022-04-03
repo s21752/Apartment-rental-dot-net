b46d75a47e8186cc61cd3a5fbb70ee7ccf64a38b
brightcezarygraban <cezary@brightinventions.pl>
2022-04-03 22:41:59 +0200

    Finished lab 2

---
+++ ApartmentRental.Core/Entities/Apartment.cs
@@ -0,0 +1,21 @@
+namespace ApartmentRental.Core.Entities;
+
+public class Apartment : BaseEntity
+{
+    public decimal RentPrice { get; set; }
+    public int RoomQuantity { get; set; }
+    public int HomeSquareMeterSize { get; set; }
+    public int Floor { get; set; }
+    public bool HasElevator { get; set; }
+
+    public int LandlordId { get; set; }
+    public Landlord Landlord { get; set; }
+
+    public int TenantId { get; set; }
+    public Tenant Tenant { get; set; }
+
+    public int AddressId { get; set; }
+    public Address Address { get; set; }
+
+    public IEnumerable<Image> Images { get; set; }
+}
